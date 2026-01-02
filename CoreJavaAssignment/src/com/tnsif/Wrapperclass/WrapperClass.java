7fada466dbc90824d62fbf3556e40992c8815ebb
Keerthana04-R <rkeerthana0404@gmail.com>
2026-01-02 17:36:56 +0530

    WrapperClass

---
+++ CoreJavaAssignment/src/com/tnsif/Wrapperclass/WrapperClass.java
@@ -0,0 +1,23 @@
+package com.tnsif.Wrapperclass;
+
+public class WrapperClass {
+
+		public static void main(String[] args) {
+		
+			int x = 50;
+			
+			Integer y = Integer.valueOf(x);
+			
+			
+			System.out.println(y);
+			
+			
+			int z = y;
+			
+			System.out.println(z);
+
+		}
+
+	}
+
+	
