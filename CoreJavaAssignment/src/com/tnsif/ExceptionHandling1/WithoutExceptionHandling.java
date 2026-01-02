20fddf7bf7f681429380068b43205c76c8589f3a
Keerthana04-R <rkeerthana0404@gmail.com>
2026-01-02 17:55:57 +0530

    ExceptionHandling1

---
+++ CoreJavaAssignment/src/com/tnsif/ExceptionHandling1/WithoutExceptionHandling.java
@@ -0,0 +1,14 @@
+package com.tnsif.ExceptionHandling1;
+
+public class WithoutExceptionHandling {
+
+
+		public static void main(String[] args) {
+			System.out.println("the program continues....");
+			int data=100/0;
+			System.out.println("the program continues....");
+		}
+		
+	}
+
+	
